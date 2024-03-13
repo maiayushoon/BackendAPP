4e1dd3d958312390485cd8d86f22133afeef87e6
maiayushhoon <ayush92002@gmail.com>
2024-03-13 17:27:13 +0530

    multer and cloudinary setup

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,29 @@
+import { v2 as cloudinary } from 'cloudinary';
+import fs from 'fs';  //fileSystem
+
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+
+const uploadOnCloudinary = async (localFilePath) => {
+    try {
+        if (!localFilePath) return null
+        //upload the file on Cloudinary
+        const response = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: "auto"
+        })
+        //file has been uploaded successfully
+        console.log("FILE IS UPLOADED ON CLOUDINARY", response.url)
+        return response;
+    } catch (error) {
+        fs.unlinkSync(localFilePath); //delete local image after uploading it to clodinary
+        return null;
+        // throw error;
+    }
+}
+
+export { uploadOnCloudinary }
